67a5444ff7ea0085aefeff4a090a1541bfd43545
JoaoCosme <49664603+JoaoCosme@users.noreply.github.com>
2022-04-24 11:44:07 -0300

    Feature Adicionar Consultas (#7)
    * Ajustado banco
    
    * Adicionado cliente 1 como nao fiel a ricardo
    
    * Adicionado pasta para diagrama
    
    * Adicionado consulta NOT exists
    
    * Adiciona décima consulta
    
    * Ajustado consultas

--- scripts/consultas.sql
+++ scripts/consultas.sql
@@ -1,25 +1,159 @@
--- 1
+-- 1 Como um lojista, quero saber todos os produtos vendidos por minhas filiais
 
-select p.nome, pv.preco , p.modelo 
-from produto_vendido_por_filial pv join produto p using(codp) 
-where pv.nome = 'Ricardo Eletro' and numero_filial = '1'
+select
+	pv.numero_filial ,
+	p.nome,
+	pv.preco ,
+	p.modelo
+from
+	produto_vendido_por_filial pv
+join produto p
+		using(codp)
+where
+	pv.nome = 'Ricardo Eletro';
 
--- 2
-select nome ,numero_filial, sum(pc.preco) 
-from filial_lojista fl natural join produto_vendido pv join produtos_carrinho pc using(codp) join carrinho c using(codcar,codc) 
-where c.finalizado = true
-group by nome, numero_filial 
-having sum(pc.preco) > 500
+-- 2 Como um lojista, quero saber quais filiais estao com um rendimento acima de um certo valor
+select
+	nome ,
+	numero_filial,
+	sum(pc.preco)
+from
+	filial_lojista fl natural
+join produto_vendido pv
+join produtos_carrinho pc
+		using(codpv)
+join carrinho c
+		using(codcar,
+	codc)
+where
+	c.finalizado = true
+	and nome = 'Ricardo Eletro'
+group by
+	nome,
+	numero_filial
+having
+	sum(pc.preco) > 4000;
+-- 3 Quero premiar meus clientes fieis, ou seja, clientes que apenas compraram comigo
+select
+	distinct c.codu as cliente_fiel
+from
+	cliente c
+join pedido p
+		using(codc)
+join carrinho c2
+		using(codcar, codc)
+join produtos_carrinho pc
+		using(codcar, codc)
+join produto_vendido pv
+		using(codpv)
+join filial_lojista
+		using (codfil)
+where
+	c.codu not in (
+	select
+		c.codu
+	from cliente c
+	join pedido p
+			using(codc)
+	join carrinho c2
+			using(codcar,
+		codc)
+	join produtos_carrinho pc
+			using(codcar,
+		codc)
+	join produto_vendido pv
+			using(codpv)
+	join filial_lojista fl
+			using (codfil)
+	where
+		fl.nome <> 'Ricardo Eletro'
+);
 
--- 3
-select distinct u1.codu as cliente_fiel
-from usuario u1 natural join cliente c join pedido p using(codc) join carrinho c2 using(codcar,codc) join produtos_carrinho pc using(codcar,codc) join produto_vendido pv using(codpv)
-	join filial f using (codfil) join lojista l using(codloj) join usuario u2 on l.codu = u2.codu
-where u1.codu not in (
-	select u.codu 
-	from usuario u natural join cliente c join pedido p using(codc) join carrinho c2 using(codcar,codc) join produtos_carrinho pc using(codcar,codc) join produto_vendido pv using(codpv)
-	join filial f using (codfil) join lojista l using(codloj) join usuario u2 on l.codu = u2.codu
-	where u2.nome  <> 'Ricardo Eletro'
-)
+--4 - Quero saber todos os lojistas que possuem filias apenas no RS(ou um estado especifico)
 
--- adicionar um carrinho para usuario 1 nao aparecer
+select
+	distinct nome as lojista
+from
+	filial_lojista
+join endereco e
+		using(code)
+where
+	codfil not in (
+	select
+		codfil
+	from
+		filial f2
+	join endereco e2
+			using(code)
+	where
+		uf <> 'rs'
+		);
+
+--Consulta com NOT EXISTS (revisar essa aqui com cuidado)
+--5 - Quero saber se existe um {cliente} que nao realizou compras num {lojista}
+	
+	
+select distinct u.codu 
+from
+	usuario u 
+join cliente c using(codu)
+join pedido p using(codc)
+join carrinho  using(codcar, codc)
+join produtos_carrinho pc using(codcar, codc)
+join produto_vendido pv using(codpv)
+join filial f using(codfil)
+where not exists (
+	select *
+	from 
+		filial_lojista
+	join produto_vendido pv2 using(codfil)
+	join produtos_carrinho pc2 using(codpv)
+	join carrinho c2 using(codcar, codc)
+	where c2.finalizado = true and c2.codc = c.codc 
+		and nome = 'Tche Eletro'
+);
+
+
+--6 - Quero saber todos os produtos comprados pro mim(um cliente) 
+
+select p.nome , p.modelo , p.fornecedor , pc.preco, pc.data_compra
+from produtos_comprados pc join produto_vendido pv using(codpv) join produto p using(codp)
+where codc='1';
+
+
+--7 - Quero saber se já comprei um {produto} antes
+
+select count(pv.codp) > 0
+from produtos_comprados pc join produto_vendido pv using(codpv)
+where codc='1' and codp = '1';
+
+
+--8 - Eu como um cliente quero poder ver todos os produtos vendidos por um {fornecedor}
+
+select p.nome, p.modelo 
+from produto p join fornecedor f on p.fornecedor = f.nome 
+where fornecedor = 'samsung';
+
+--9 - Desejando saber a abraangencia de um {fornecedor}, quero todas as {categorias} que ele vende
+
+select c.nome 
+from produto p join fornecedor f on p.fornecedor = f.nome  join categoria_produto cp using(codp)
+join categoria c using(codcat)
+where fornecedor = 'adidas'
+group by c.nome ;
+
+
+--10 - Desejando saber qual filial esta mais perto do cliente, quero saber {filail} no mesmo uf que o {cliente}
+
+select numero_filial 
+from 
+	filial_lojista
+join endereco e using(code) 
+where nome = 'Tche Produtos' and e.uf in 
+(select distinct e2.uf  
+from usuario u2
+join cliente c using(codu)
+join endereco_cliente ec using(codc)
+join endereco e2 using(code));
+
+
